02b5631e6cf0b3242b53c67368c6f1b6927c114d
tanque <paoletti.francesco@gmail.com>
2023-10-08 01:09:34 +0200

    add app globals and small app info snippet at launch

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+mod globals;
+
+fn app_info() {
+    println!("#################################################################");
+    println!("application host: {:?}", globals::HOST);
+    println!("application port: {:?}", globals::PORT);
+    println!("application data folder: {:?}", globals::APP_DATA_ROOT);
+    println!(
+        "application database folder: {:?}",
+        globals::DATABASE_FOLDER
+    );
+    println!("application database name: {:?}", globals::DATABASE_NAME);
+    println!("application database path: {:?}", globals::DATABASE_PATH);
+    println!("#################################################################");
+}
+
 fn main() {
-    println!("Hello, develop!");
+    println!("Hello, develop!\n");
+    app_info();
 }
