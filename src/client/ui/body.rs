279ff6e6a352138342a96f5ae4e8d53a69912bad
tanque <paoletti.francesco@gmail.com>
2023-10-08 17:30:15 +0200

    (#3) feat(app): add top-fixed navbar

--- src/client/ui/body.rs
+++ src/client/ui/body.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 
 use html_to_string_macro::html;
 
-use crate::client::ui::{Component, Home};
+use crate::client::ui::{Component, Home, Navbar};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Body {}
@@ -12,7 +12,8 @@ pub struct Body {}
 impl Component for Body {
     fn render(&self) -> String {
         let result = html!(
-            <body>
+            <body class="has-navbar-fixed-top">
+                {Navbar::new().render()}
                 {Home::new().render()}
             </body>
         );
--- src/client/ui/mod.rs
+++ src/client/ui/mod.rs
@@ -2,10 +2,12 @@ mod body;
 mod component;
 mod head;
 mod home;
+mod navbar;
 mod page;
 
 pub use body::Body;
 pub use component::Component;
 pub use head::Head;
 pub use home::Home;
+pub use navbar::Navbar;
 pub use page::Page;
